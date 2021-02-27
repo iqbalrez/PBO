3659408da6eae171b28f644d5529d959b8200f01
ridife <65213950+ridiferd@users.noreply.github.com>
2021-02-27 14:36:29 +0700

    Commit for console app that use car OOP version

---
+++ ConsoleCarOOP/ConsoleCarOOP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleCarOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tampilan UI            
+            Console.WriteLine("Masukkan Merk Kendaraanmu?");
+            string merk = Console.ReadLine();
+            // model
+            Console.WriteLine("Masukkan Model Kendaraan?");
+            string model = Console.ReadLine();
+            // HP
+            Console.WriteLine("Masukkan Horse Power Kendaraan Anda");
+            double hp = Convert.ToDouble(Console.ReadLine());
+            // instancing
+            Vehicle car = new Vehicle(merk,model,hp);
+            // call procedure 
+            car.ShowVehicleInformation();
+            // call function 
+            double velocity = car.CalculateOptimumVelocity(hp);
+          
+            Console.WriteLine("Kecepatan Optimum Mobil ini adalah {0}", velocity );
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ ConsoleCarOOP/ConsoleCarOOP/Vehicle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleCarOOP
+{
+    class Vehicle
+    {
+
+        // attribute, field, variables
+        string brand;
+        string model;
+        double horsePower;
+
+        
+        bool isTurbo;
+
+
+        // function, method, procedure, behavior
+
+        // constructor
+        public Vehicle(string brand, string model, double hp)
+        {
+            this.brand = brand;
+            this.model = model;
+            horsePower = hp;
+        }
+
+        // ini procedure
+        public void ShowVehicleInformation()
+        {
+            Console.WriteLine("{0} - {1} that has {2} HP", brand, model, horsePower);
+        }
+
+        // ini function 
+        public double CalculateOptimumVelocity(double horsePower)
+        {
+            double velocity = 0;
+            // velocity non turbo
+            velocity = (horsePower/ 4) * 1.5 + 100;
+
+            // velocity turbp 
+            // velocity = velocity + 0.1 * velocity;
+
+
+            return velocity;
+        }
+    }
+}
